5d466922ac79d18ba13859eb95fa12e4fa86c8d2
Miguel López <miguellopez@vitalware.com>
2019-07-01 14:46:14 -0500

    create home module

---
+++ src/lib/home/components/index.ts
@@ -0,0 +1,5 @@
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
+
+export const components = [HomeLayoutComponent];
+
+export * from './home-layout/home-layout.component';
