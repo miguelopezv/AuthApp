2d70d4a0a63dc35ccea396aff1b637ed5bbd4d95
Miguel López <miguellopez@vitalware.com>
2019-07-01 15:13:57 -0500

    add routes for all modules

--- src/lib/app/app-routing.module.ts
+++ src/lib/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'pricing',
+    loadChildren: () =>
+      import('../prices/prices.module').then(m => m.PricesModule)
+  },
+  {
+    path: 'protected',
+    loadChildren: () =>
+      import('../protected/protected.module').then(m => m.ProtectedModule)
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/lib/home/home.module.ts
+++ src/lib/home/home.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import * as fromComponents from './components';
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES: Routes = [
+  { path: '', component: fromComponents.HomeLayoutComponent }
+];
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule.forChild(ROUTES)]
 })
 export class HomeModule {}
