f98c243007a5b312f2929d081d6055c9d2cf318e
Miguel López <miguellopez@vitalware.com>
2019-07-01 15:14:24 -0500

    add router-outlet and app title fix

--- src/lib/protected/components/protected-layout/protected-layout.component.ts
+++ src/lib/protected/components/protected-layout/protected-layout.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: 'app-protected-layout',
+  templateUrl: './protected-layout.component.html',
+  styleUrls: ['./protected-layout.component.scss']
 })
 export class ProtectedLayoutComponent implements OnInit {
   constructor() {}
