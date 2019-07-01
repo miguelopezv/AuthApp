74f5d9ad2b446c753441401591bc600774969029
Miguel López <miguellopez@vitalware.com>
2019-07-01 15:25:01 -0500

    update routermodule location

--- src/lib/layout/layout.module.ts
+++ src/lib/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import * as fromComponents from './components';
+import { AppRoutingModule } from '../app/app-routing.module';
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
   exports: [...fromComponents.components]
 })
 export class LayoutModule {}
