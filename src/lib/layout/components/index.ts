839e3e7cd4599f3baafb1f92ad1babf1749ec5c7
Miguel López <miguellopez@vitalware.com>
2019-07-01 22:54:34 -0500

    add login/logout buttons and logic for auth0

--- src/lib/layout/components/index.ts
+++ src/lib/layout/components/index.ts
@@ -1,5 +1,7 @@
 import { NavbarComponent } from './navbar/navbar.component';
+import { CallbackComponent } from './callback/callback.component';
 
-export const components = [NavbarComponent];
+export const components = [NavbarComponent, CallbackComponent];
 
 export * from './navbar/navbar.component';
+export * from './callback/callback.component';
--- src/lib/layout/components/navbar/navbar.component.ts
+++ src/lib/layout/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Auth0Client from '@auth0/auth0-spa-js/dist/typings/Auth0Client';
+import { AuthService } from 'src/lib/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  isAuthenticated = false;
+  profile: any;
 
-  constructor() { }
+  private auth0Client: Auth0Client;
 
-  ngOnInit() {
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    this.auth0Client = await this.authService.getAuth0Client();
+
+    this.authService.isAuthenticated.subscribe(value => {
+      this.isAuthenticated = value;
+    });
+
+    this.authService.profile.subscribe(profile => {
+      this.profile = profile;
+    });
   }
 
+  async login() {
+    await this.auth0Client.loginWithRedirect({});
+  }
+
+  logout() {
+    this.auth0Client.logout({
+      client_id: this.authService.config.client_id,
+      returnTo: window.location.origin
+    });
+  }
 }
