7e594f5a43f60cd59ddabe15ba1d02ab2bc70241
Miguel López <miguellopez@vitalware.com>
2019-07-02 01:02:52 -0500

    create and implement guard

--- src/lib/app/app-routing.module.ts
+++ src/lib/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CallbackComponent } from '../layout/components';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'protected',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('../protected/protected.module').then(m => m.ProtectedModule)
   },
--- src/lib/layout/layout.module.ts
+++ src/lib/layout/layout.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import * as fromComponents from './components';
 import { AppRoutingModule } from '../app/app-routing.module';
 import { AuthService } from '../services/auth.service';
+import { AuthGuard } from '../guards/auth.guard';
 
 @NgModule({
   declarations: [...fromComponents.components],
   imports: [CommonModule, AppRoutingModule],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   exports: [...fromComponents.components]
 })
 export class LayoutModule {}
